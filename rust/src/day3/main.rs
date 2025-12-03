39cd8cf05032ed071fd76f393989a69c99271870
Maarten Depauw <mhkdepauw@gmail.com>
2025-12-04 00:26:00 +0100

    cleaned up the code

--- rust/src/day3/main.rs
+++ rust/src/day3/main.rs
@@ -7,33 +7,30 @@ fn main() {
     println!("{}", part2(real_input.clone()));
 }
 
-fn part1(megavec: Vec<String>) -> u32 {
-    let mut max_joltage: u32 = 0;
-    for batbank in megavec.iter().map(|s| s.chars().map(|c|c.to_digit(10).unwrap()).collect::<Vec<u32>>()) {
-        let batbank_minus:Vec<u32> = batbank[..batbank.len()-1].to_owned();
-        let (d1, i1 ) = highest_non_ending_nr(batbank_minus);
-        let (d2, _) = highest_non_ending_nr(batbank[i1 + 1..].to_owned());
-        max_joltage += (d1*10) + d2;
-    }
-    max_joltage
+fn part1(megavec: Vec<String>) -> u64 {
+    highest_substring(megavec,2)
 }
 fn part2(megavec: Vec<String>) -> u64 {
-    let mut max_joltage: u64 = 0;
-    for batbank in megavec.iter().map(|s| s.chars().map(|c|c.to_digit(10).unwrap()).collect::<Vec<u32>>()) {
-        let mut batbank_max_joltage: u64 = 0;
-        let mut start_i = 0;
-        for l in 0..12 {
-            let batbank_minus:Vec<u32> = batbank[start_i..batbank.len()-(11-l)].to_owned();
-            let (d, i) = highest_non_ending_nr(batbank_minus.clone());
-            batbank_max_joltage = (batbank_max_joltage*10) + d as u64;
-            start_i += i+1;
-        }
-        max_joltage += batbank_max_joltage;
-    }
-    max_joltage
+    highest_substring(megavec,12)
 }
 
-fn highest_non_ending_nr(batbank: Vec<u32>) -> (u32, usize) {
-    let highest = batbank.iter().fold(0, |acc, x| if x > &acc { *x } else {acc});
-    (highest,batbank.iter().position(|x1| {*x1 == highest}).unwrap())
+fn highest_non_ending_nr(numvec: Vec<u32>) -> (u32, usize) {
+    let highest = numvec.iter().fold(0, |acc, x| if x > &acc { *x } else {acc});
+    (highest, numvec.iter().position(|x1| {*x1 == highest}).unwrap())
+}
+
+fn highest_substring(megavec: Vec<String>, string_length: usize) -> u64 {
+    let mut total_highest: u64 = 0;
+    for numvec in megavec.iter().map(|s| s.chars().map(|c|c.to_digit(10).unwrap()).collect::<Vec<u32>>()) {
+        let mut numvec_highest: u64 = 0;
+        let mut start_i = 0;
+        for l in 0..string_length {
+            let sliced_numvec:Vec<u32> = numvec[start_i..numvec.len()-((string_length-1)-l)].to_owned();
+            let (d, i) = highest_non_ending_nr(sliced_numvec);
+            numvec_highest = (numvec_highest *10) + d as u64;
+            start_i += i+1;
+        }
+        total_highest += numvec_highest;
+    }
+    total_highest
 }
