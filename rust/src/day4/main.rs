6fb94b54b87db32c8ba8617e6f25516df2ea2634
mhkdepauw <mhkdepauw@gmail.com>
2025-12-04 19:29:12 +0100

    part2 done

--- rust/src/day4/main.rs
+++ rust/src/day4/main.rs
@@ -1,16 +1,30 @@
 use rust::read_lines;
 
 fn main() {
-    let dummy_input: Vec<String> = "..xx.xx@x.,x@@.@.@.@@,@@@@@.x.@@,@.@@@@..@.,x@.@@@@.@x,.@@@@@@@.@,.@.@.@.@@@,x.@@@.@@@@,.@@@@@@@@.,x.x.@@@.x."
+    let dummy_input: Vec<String> = "..@@.@@@@.,@@@.@.@.@@,@@@@@.@.@@,@.@@@@..@.,@@.@@@@.@@,.@@@@@@@.@,.@.@.@.@@@,@.@@@.@@@@,.@@@@@@@@.,@.@.@@@.@."
         .split(",").map(|s|s.to_string()).collect();
-    // let real_input: Vec<String> = read_lines(4);
-    println!("{}", part1(&dummy_input));
-    println!("{}", part2(&dummy_input));
+    let real_input: Vec<String> = read_lines(4);
+    println!("{}", part1(&real_input));
+    println!("{}", part2(&real_input));
 }
 
 fn part1(megavec: &Vec<String>) -> i32 {
+    countremove_accesibles(megavec.clone().iter().map(|s| {s.chars().collect()}).collect()).0
+}
+
+fn part2(megavec: &Vec<String>) -> i32 {
+    let mut imegavec: Vec<Vec<char>> = megavec.clone().iter().map(|s| {s.chars().collect()}).collect();
+    let mut total_accessible = 0;
+    let mut last_remove = 1;
+    while last_remove != 0 {
+        (last_remove, imegavec) = countremove_accesibles(imegavec);
+        total_accessible += last_remove;
+    }
+    total_accessible
+}
+
+fn countremove_accesibles(mut imegavec: Vec<Vec<char>>) -> (i32, Vec<Vec<char>>){
     let mut accessible = 0;
-    let imegavec:Vec<Vec<char>> = megavec.iter().map(|s| {s.chars().collect()}).collect();
     for i in 0..imegavec.len() {
         let inotfirst = i > 0;
         let inotlast = i < imegavec.len()-1;
@@ -31,13 +45,12 @@ fn part1(megavec: &Vec<String>) -> i32 {
                     if jnotfirst && imegavec[i+1][j-1] == '@' { count+=1 }
                     if jnotlast && imegavec[i+1][j+1] == '@' { count+=1 }
                 }
-                if count < 4 { accessible+=1 }
+                if count < 4 {
+                    accessible+=1;
+                    imegavec[i][j] = '.';
+                }
             }
         }
     }
-    accessible
-}
-
-fn part2(megavec: &Vec<String>) -> i32 {
-    0
+    (accessible,imegavec)
 }
