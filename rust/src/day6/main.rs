2145dbc30749481e8fe6b9e6d43981e472364613
Maarten Depauw <mhkdepauw@gmail.com>
2025-12-06 23:09:50 +0100

    part1 done

--- rust/src/day6/main.rs
+++ rust/src/day6/main.rs
@@ -1,5 +1,6 @@
 use std::fs;
 use std::fs::read_to_string;
+use rust::read_lines;
 
 fn main() {
     let dummy_input: Vec<String> = read_to_string("src/day6/dummy_input.txt")
@@ -7,19 +8,26 @@ fn main() {
         .lines()
         .map(|l| l.to_string())
         .collect();
-    println!("{}", part1(&dummy_input));
+    let real_input: Vec<String> = read_lines(6);
+    println!("{}", part1(&real_input));
     println!("{}", part2(&dummy_input));
 }
 
-fn part1(megavec: &Vec<String>) -> i32 {
-    0
+fn part1(megavec: &Vec<String>) -> i64 {
+    let mut total_count: i64 = 0;
+    let (ops, num_list): (Vec<String>,Vec<Vec<i64>>) = to_op_list(megavec);
+    for i in 0..num_list.len() {
+        if ops[i] == "*" { total_count += num_list[i].iter().product::<i64>()}
+        else { total_count += num_list[i].iter().sum::<i64>() }
+    }
+    total_count
 }
 
 fn part2(megavec: &Vec<String>) -> i32 {
     0
 }
 
-fn to_op_list(megavec: Vec<String>) -> (Vec<String>, Vec<Vec<i32>>) {
+fn to_op_list(megavec: &Vec<String>) -> (Vec<String>, Vec<Vec<i64>>) {
     let processed: Vec<Vec<String>> = megavec
         .iter()
         .map(|s| {
@@ -29,9 +37,9 @@ fn to_op_list(megavec: Vec<String>) -> (Vec<String>, Vec<Vec<i32>>) {
                 .collect::<Vec<String>>()
         })
         .collect();
-    let mut transposed_m: Vec<Vec<i32>> = vec![];
+    let mut transposed_m: Vec<Vec<i64>> = vec![];
     for i in 0..processed[0].len() {
-        let mut transposed: Vec<i32> = vec![];
+        let mut transposed: Vec<i64> = vec![];
         for j in 0..processed.len() - 1 {
             transposed.push(processed[j][i].parse().unwrap());
         }
